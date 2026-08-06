978d4ec7bc93bf454790b87f9f57e2c38d59a023
Deveshkirad <deveshkirad4@gmail.com>
2026-08-06 18:10:23 +0530

    Added hero section on index page

---
+++ frontend/assets/js/tailwind-config.js
@@ -0,0 +1,12 @@
+// Customizing Tailwind with theme colors and fonts
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        "text-primary": "var(--color-text-primary)",
+        "text-secondary": "var(--color-text-secondary)",
+      },
+    },
+  },
+};
